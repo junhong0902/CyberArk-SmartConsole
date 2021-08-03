ff4068ccae0ab878e417b1bf171cb50da57f3313
Jun Hong Choo <jh.choo0902@gmail.com>
2021-08-03 14:49:32 +0800

    Update psm-create-smartconsole-xml.ps1

--- smartconsole/psm-create-smartconsole-xml.ps1
+++ smartconsole/psm-create-smartconsole-xml.ps1
@@ -13,6 +13,7 @@ $strUsername = $args[0]
 $strAddress = $args[1]
 $HexPath = $args[2]
 $HexPassword = $args[3]
+$strActionName = $args[4]
 
 # Custom functions
 function hextostring
@@ -30,37 +31,51 @@ function hextostring
     # Return result
     return (-join $hex)
 }
+switch($strActionName)
+{
+	'createxml'
+    {
+		<# Testing values - To be removed in Production deployment
+		$strUsername = "chkpointadm"
+		$strAddress = "1.1.1.2"
+		$strPassword = "Cyberark1"
+		$smartConsoleFile = "C:\Program Files (x86)\CyberArk\PSM\Components\smartconsole\temp\file.xml"
+		#>
 
-# Convert HexPassword & HexPath to String
-$strPassword = hextostring $HexPassword
-$smartConsoleFile = hextostring $HexPath
+
+		# Convert HexPassword & HexPath to String
+		$strPassword = hextostring $HexPassword
+		$smartConsoleFile = hextostring $HexPath
 
 
-$objXmlWriter = New-Object System.Xml.XmlTextWriter($smartConsoleFile,$null)
+		$objXmlWriter = New-Object System.Xml.XmlTextWriter($smartConsoleFile,$null)
 
-# Formatting
-$objXmlWriter.Formatting = 'Indented'
-$objXmlWriter.Indentation = 1
-$objXmlWriter.IndentChar = "`t"
+		# Formatting
+		$objXmlWriter.Formatting = 'Indented'
+		$objXmlWriter.Indentation = 1
+		$objXmlWriter.IndentChar = "`t"
 
-# Write the header and set XLS statements
-$objXmlWriter.WriteStartDocument()
+		# Write the header and set XLS statements
+		$objXmlWriter.WriteStartDocument()
 
-# Write the content to xml
-$objXmlWriter.WriteStartElement('RemoteLaunchParemeters')
-$objXmlWriter.WriteElementString('Username', $strUsername)
-$objXmlWriter.WriteElementString('ServerIP', $strAddress)
-$objXmlWriter.WriteElementString('Password', $strPassword)
-$objXmlWriter.WriteElementString('ReadOnly', 'False')
-$objXmlWriter.WriteElementString('CloudDemoMode', 'False')
-$objXmlWriter.WriteEndElement()
+		# Write the content to xml
+		$objXmlWriter.WriteStartElement('RemoteLaunchParemeters')
+		$objXmlWriter.WriteElementString('Username', $strUsername)
+		$objXmlWriter.WriteElementString('ServerIP', $strAddress)
+		$objXmlWriter.WriteElementString('Password', $strPassword)
+		$objXmlWriter.WriteElementString('ReadOnly', 'False')
+		$objXmlWriter.WriteElementString('CloudDemoMode', 'False')
+		$objXmlWriter.WriteEndElement()
 
-# Finalize the xml file
-$objXmlWriter.WriteEndDocument()
-$objXmlWriter.Flush()
-$objXmlWriter.Close()
-
-# Wait 10 sec for autoIT to load the xml and delete it after. Delete recursively to make sure the folder is empty
-Sleep(10)
-write-host (get-item $smartConsoleFile).Directory.FullName
-get-childitem (get-item $smartConsoleFile).Directory.FullName -include *.xml -recurse | remove-item -Force -Recurse
+		# Finalize the xml file
+		$objXmlWriter.WriteEndDocument()
+		$objXmlWriter.Flush()
+		$objXmlWriter.Close()
+	}
+	'deletexml'
+	{
+		# Delete the xml files recursively
+		#write-host (get-item $smartConsoleFile).Directory.FullName
+		get-childitem (get-item $smartConsoleFile).Directory.FullName -include *.xml -recurse | remove-item -Force
+	}
+}
